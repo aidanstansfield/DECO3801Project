1c40eadd34d3200365b3311dd5d4264905b75c6c
Danie Grobbelaar <s4436755@student.uq.edu.au>
2019-05-29 18:33:07 +1000

    Removed unneeded comments.

--- allocation/group_size_generator.py
+++ allocation/group_size_generator.py
@@ -194,161 +194,30 @@ def determine_group_numbers(no_people, min_size, ideal_size, max_size):
         attempt_to_form_ideal_groups(no_people, ideal_size)
     formed_groups.extend(ideal_groups)
 
-    #no_ideal_groups, remaining_people = divmod(no_people, ideal_size)
-
-    #for i in range(no_ideal_groups):
-    #    formed_groups.append(Group(ideal_size))
-    
     # Now, remaining_people < ideal_size
     # We have two options, we can either form smaller groups, or add the
     # remaining people to the existing groups
-    
     remaining_people, smaller_groups = \
         attempt_to_form_smaller_groups(remaining_people, ideal_size, min_size)
     formed_groups.extend(smaller_groups)
 
-
-    #current_size = ideal_size - 1
-    #while current_size > min_size:
-        # See if we can make a group of this size. If we can, then
-        # make it since it will maximise the number of ideal groups
-    #    if current_size == remaining_people:
-    #        formed_groups.append(Group(current_size))
-    #        remaining_people = 0
-    #    current_size -= 1
-    
-    #if remaining_people > 0:
-        # We've failed to create smaller groups within acceptable
-        # parameters that use all remaining people. So instead, we 
-        # try to increase the size of the other groups. 
-
-        # First, we see if we can add all of the remaining people
-        # into a single ideal group. This maximises the number of
-        # ideal groups
-    #    for group in formed_groups:
-    #        if remaining_people + group.get_size() <= max_size:
-    #            group.alter_size(remaining_people)
-    #            remaining_people = 0
-    
     remaining_people = \
         attempt_to_add_to_existing_groups(remaining_people, formed_groups, max_size)
 
-    #if remaining_people > 0:
-        # We can't add all the people into a single ideal group
-        # So instead we try to add as many people into the ideal
-        # groups systematically
-    #    for group in formed_groups:
-    #        space_left = max_size - group.get_size()
-
-    #        while space_left > 0 and remaining_people > 0:
-    #            group.increment_size()
-    #            remaining_people -= 1
-    #            space_left = max_size - group.get_size()
-
+    # The only option left is to try and break people out of
+    # groups in order to form another group of minimum size
     remaining_people = \
         attempt_to_form_min_sized_group_from_existing(remaining_people, formed_groups, min_size)
 
+    # If we still have people left over, then it's not possible
     if remaining_people > 0:
         raise ImpossibleConstraintsError()
     
-    #if remaining_people > 0:
-        # If we're in here, it means that we've failed to make
-        # smaller and larger groups and still have people remaining.
-        # The only option left is to try and break people out of
-        # groups in order to form another group of minimum size
-    #    people_needed = min_size - remaining_people
-    #    people_that_can_be_moved = 0
-
-        # We need to know whether or not we are able to remove
-        # enough people to form this new group
-    #    group_indices = []
-    #    for index, group in enumerate(formed_groups):
-    #        if group.get_size() > min_size:
-    #            max_people_to_move = group.get_size() - min_size
-    #            people_shifted = min(max_people_to_move, people_needed - people_that_can_be_moved)
-
-    #            if people_shifted > 0:
-    #                people_that_can_be_moved += people_shifted
-    #                group_indices.append((index, people_shifted))
-
-    #            if people_that_can_be_moved >= people_needed:
-    #                break
-        
-        # If we dont have enough people, then it's impossible.
-        # Otherwise, we remove the people needed
-    #    if people_that_can_be_moved < people_needed:
-    #        raise ValueError("Impossible Constraints")
-    #    else:
-    #        for index, amount in group_indices:
-    #            formed_groups[index].alter_size(-amount)
-    #        formed_groups.append(Group(min_size))
-
-    #final_result = {}
-    
-    #for group in formed_groups:
-    #    if group.get_size() not in final_result:
-    #        final_result[group.get_size()] = 1
-    #    else:
-    #        final_result[group.get_size()] += 1
-
-    #return final_result   
     return collate_formed_groups(formed_groups) 
                 
 #print(determine_group_numbers(20, 3, 4, 5))
 #print(determine_group_numbers(5531, 18, 24, 38))
 #print(determine_group_numbers(10, 4, 4, 5))
-#print(determine_group_numbers(56, 3, 54, 55))
+#print(determine_group_numbers(56, 24, 54, 55))
+#print(determine_group_numbers(1000, 78, 79, 80))
 #print(determine_group_numbers(15, 8, 9, 10))
-
-"""
-    groups = []
-
-    # We naively try to create as many ideal size groups
-    no_ideal_groups, remaining_people = divmod(no_people, ideal_size)
-
-    # If the min_size, ideal_size and max_size are the same and we
-    # have people left over, then it's not possible
-    if min_size == ideal_size and ideal_size == max_size and remaining_people != 0:
-        raise ValueError("Impossible constraints")
-    else:
-        for i in range(no_ideal_groups):
-            groups.append(Group(ideal_size))
-   
-    # With the remaining number of people, we can try and append them to the existing
-    # groups of ideal size if it doesn't exceed the max size
-    remaining_range = range(remaining_people)
-    for i in remaining_range:
-        if groups[i % len(groups)].size < max_size:
-            groups[i % len(groups)].increment_size()
-            remaining_people -= 1
-   
-    # If there are still people remaining, we try and form a new group of that size
-    # If it isn't in the wanted range, we try and extract enough people to make
-    # an ideal or minimum sized team without breaking other groups
-    if remaining_people != 0:
-        if remaining_people == min_size:
-            groups.append(Group(remaining_people))
-        elif remaining_people < min_size:
-            people_to_extract = min_size - remaining_people
-            people_extracted = 0
-
-            group_copy = groups.copy()
-            for i in range(people_to_extract):
-                if groups[i % len(groups)].size > min_size:
-                    groups[i % len(groups)].decrement_size()
-                    people_extracted += 1
-                if people_extracted == people_to_extract:
-                    break
-            
-            if people_extracted == people_to_extract:
-                for i in range(people_to_extract):
-                    groups[i % len(groups)].decrement_size()
-                groups.append(Group(min_size))
-            else:
-                raise ValueError("Impossible Constraints")
-        else:
-            groups.append(Group(remaining_people))
-
-    print(groups)
-
-"""
