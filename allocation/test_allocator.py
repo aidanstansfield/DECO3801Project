81618f9a02d7d28d65e9d6631257cdbab4577c87
Richy <Deepfriedice@gmail.com>
2019-06-06 17:57:37 +1000

    Expand allocator unit tests

--- allocation/test_allocator.py
+++ allocation/test_allocator.py
@@ -3,7 +3,15 @@ import allocator
 import constraints
 
 
-# Make teams based on grouping "True" and "False"
+# Tests if list "a" is a permutation of list "b"
+def is_permutation(a, b):
+	for element in set(a).union(set(b)):
+		if a.count(element) != b.count(element):
+			return False
+	return True
+
+
+# Make teams based on grouping "True" or "False"
 class DummyConstraint(constraints.Constraint):
 	def __init__(self):
 		super().__init__("dummy", None, "dummy", 0)
@@ -18,14 +26,20 @@ class DummyConstraint(constraints.Constraint):
 
 class TestSplitTeams(unittest.TestCase):
 	
-	# Ensure teams are the correct size
-	def test_size(self):
+	# Ensure that the right number of teams are created
+	def test_number(self):
 		students = range(6)
 		sizes = {2:3}
 		teams = list(allocator.split_teams(students, sizes))
 		self.assertEqual(len(teams), 3)
+	
+	# Ensure teams are the correct size
+	def test_size(self):
+		students = range(14)
+		sizes = {3:3, 5:1}
+		teams = list(allocator.split_teams(students, sizes))
 		for team in teams:
-			self.assertEqual(len(team), 2)
+			self.assertTrue(len(team) in (3,5))
 	
 	# Ensure students show up once and only once
 	def test_appearance(self):
@@ -34,10 +48,8 @@ class TestSplitTeams(unittest.TestCase):
 		teams = list(allocator.split_teams(students, sizes))
 		appeared = []
 		for team in teams:
-			for student in team:
-				self.assertTrue(student in students)
-				self.assertFalse(student in appeared)
-				appeared.append(student)
+			appeared += team
+		self.assertTrue(is_permutation(appeared, students))
 
 
 class TestTeamBuilderMethods(unittest.TestCase):
@@ -50,11 +62,8 @@ class TestTeamBuilderMethods(unittest.TestCase):
 		teambuilder.move()
 		appeared = []
 		for team in teambuilder.teams():
-			for student in team:
-				self.assertTrue(student in students)
-				self.assertFalse(student in appeared)
-				appeared.append(student)
-		self.assertEqual(len(appeared), len(students))
+			appeared += team
+		self.assertTrue(is_permutation(appeared, students))
 	
 	# Test that an allocation with no issues has an energy  of zero
 	def test_zero_energy(self):
@@ -76,6 +85,20 @@ class TestTeamBuilderMethods(unittest.TestCase):
 		team_sizes = {4:1}
 		teambuilder = allocator.TeamBuilder(student_info.keys(), team_sizes, student_info, [DummyConstraint()])
 		self.assertEqual(teambuilder.energy(), 4)
+
+
+# Test that complete allocations will occur successfully
+# WARNING: allocate_teams() is not deterministic. It's possible, but incredibly unlikely, for this test to fail by chance.
+# Also, simanneal doesn't seem to have an option for turning off printing to stderr.
+class TestAllocation(unittest.TestCase):
+	def test_allocate_teams(self):
+		student_info = {"A":True, "B":False, "C":True, "D":False, "E":True, "F":True, "G":False, "H":False}
+		teams = allocator.allocate_teams(4, 4, 4, student_info, [DummyConstraint()])
+		for team in teams.keys():
+			if 'A' in team:
+				self.assertTrue(is_permutation(team, ["A", "C", "E", "F"]))
+			else:
+				self.assertTrue(is_permutation(team, ["B", "D", "G", "H"]))
 
 
 if __name__=='__main__':
