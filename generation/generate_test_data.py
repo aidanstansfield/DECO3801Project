99305270818c99474d3e4b7eddf324a4949ba4f8
Dillon Steyl <dillon.steyl.98@outlook.com>
2019-08-16 21:25:33 +1000

    Added function to generate POC data

--- generation/generate_test_data.py
+++ generation/generate_test_data.py
@@ -4,6 +4,15 @@ import random
 import sys
 
 
+# Constants
+DEFAULT_PREFERENCES = {
+	"ui":         0.2,
+	"networking": 0.25,
+	"graphics":   0.3,
+	"gameplay":   0.4,
+}
+
+
 def gen_student_id():
 	generated = set()
 	length = 8
@@ -60,28 +69,54 @@ def gen_students(count, other_gens):
 	return students
 
 
-count = 16
-if len(sys.argv) > 1:
-	try:
-		count = int(sys.argv[1])
-	except ValueError:
-		pass
+def gen_poc_data(count, fnames, lnames, age_range=(18,27), preferences=DEFAULT_PREFERENCES):
+	"""Generates a simple set of proof of concept data.
+	Generates name, age and preferences.
+	
+	Args:
+		count (int): The number of instances to generate
+		fnames (str): String containing path to .txt file with first names to choose from
+		lnames (str): String containing path to .txt file with last names to choose from
+		age_range (tuple(int, int)): A tuple (lower, upper) defining the range of ages to generate from
+		preferences (dict, optional): Dictionary defining preference probabilities. Defaults to DEFAULT_PREFERENCES.
+			Example:
+			{
+				"ui":         0.2,
+				"networking": 0.25,
+				"graphics":   0.3,
+				"gameplay":   0.4,
+			}
 
-preferences = {
-	"ui":         0.2,
-	"networking": 0.25,
-	"graphics":   0.3,
-	"gameplay":   0.4,
-	}
+	Returns:
+		dict: [description]
+	"""
+	students = gen_students(count, {
+			"name": gen_student_name(fnames, lnames),
+			"age": gen_student_age(age_range[0], age_range[1]),
+			"preferences": gen_student_preferences(preferences),
+			})
 
-# Paths to name files. Not great.
-fnames = os.path.join(os.path.dirname(sys.argv[0]), "fnames.txt")
-lnames = os.path.join(os.path.dirname(sys.argv[0]), "lnames.txt")
+	return students
 
-students = gen_students(count, {
-		"name": gen_student_name(fnames, lnames),
-		"age": gen_student_age(17,27),
-		"preferences": gen_student_preferences(preferences),
-		})
 
-print(json.dumps(students))
+if __name__ == "__main__":
+	count = 16
+	if len(sys.argv) > 1:
+		try:
+			count = int(sys.argv[1])
+		except ValueError:
+			pass
+
+	preferences = DEFAULT_PREFERENCES
+
+	# Paths to name files. Not great.
+	fnames = os.path.join(os.path.dirname(sys.argv[0]), "fnames.txt")
+	lnames = os.path.join(os.path.dirname(sys.argv[0]), "lnames.txt")
+
+	students = gen_students(count, {
+			"name": gen_student_name(fnames, lnames),
+			"age": gen_student_age(17,27),
+			"preferences": gen_student_preferences(preferences),
+			})
+
+	print(json.dumps(students))
