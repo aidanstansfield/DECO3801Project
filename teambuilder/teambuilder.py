bb8d7b8917cceeb5a36ffee4ef95b7400acab19a
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-08-28 17:23:35 +1000

    teambuilder changes

--- teambuilder/teambuilder.py
+++ teambuilder/teambuilder.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, request, abort
+import json
 
 app = Flask(__name__)
 
+@app.before_first_request
+def check_auth():
+    if 'staff' not in request.headers.get('X-Uq-User-Type').lower:
+        abort(403)
+
+@app.errorhandler(403)
+def denied():
+    # make a custom 403
+    return "Permission Denied"
+
 @app.route('/')
 def home():
-	return "Go Away"
+    return str(request.headers)
 
 if __name__ == "__main__":
 	host = "0.0.0.0"
