841191efc04868b545bee20d0c1ddb1865dc8ded
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-08-28 18:53:39 +1000

    teambuilder changes, better auth

--- teambuilder/teambuilder.py
+++ teambuilder/teambuilder.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
-from flask import Flask, request, abort
+from flask import Flask, request, abort, send_from_directory
 import json
+import werkzeug.exceptions
+import os
 
 app = Flask(__name__)
 
-@app.before_first_request
+explicitly_allowed_users = ['s4434177', 's4200694', 's4317687', 's4386414', 
+    's4432329', 's4436755']
+
+@app.before_request
 def check_auth():
-    if 'staff' not in request.headers.get('X-Uq-User-Type').lower:
-        abort(403)
+    if (request.headers.get('X-Uq-User-Type') != None and \
+            'staff' in request.headers.get('X-Uq-User-Type').lower) or \
+            (request.headers.get('X-Uq-User') != None and \
+            request.headers.get('X-Uq-User').lower in explicitly_allowed_users):
+        return
+    # if we haven't returned, user is not allowed
+    abort(403)
 
 @app.errorhandler(403)
-def denied():
-    # make a custom 403
-    return "Permission Denied"
+def denied(e):
+    # make a custom 403 html file
+    return "Permission Denied", 403
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def home():
@@ -21,5 +36,5 @@ def home():
 if __name__ == "__main__":
 	host = "0.0.0.0"
 	port = 8080
-	app.run(host, port)
+	app.run(host, port, debug=True)
 
