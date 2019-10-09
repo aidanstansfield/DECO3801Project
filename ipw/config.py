b3a977337f645cd3089a978d32b11b0c4da5e5df
Aidan Stansfield <a.stansfield@uq.net.au>
2019-10-09 14:31:41 +0000

    These are really really messy/bad but i need to go to bed. Will fix up. Database now works for ipw.

--- ipw/config.py
+++ ipw/config.py
@@ -4,5 +4,5 @@ class Config:
     # General
     SECRET_KEY = '\xb7)\xa8\x9d\xd3\xa1\xeaG[+\xe3\xfa\xe0\xb2\xe2j'
     # Database
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://allotech:allotech@localhost/ipw'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://allotech:allotech@localhost/ipw'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- ipw/ipw.py
+++ ipw/ipw.py
@@ -5,9 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime as dt
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+#app.config.from_object('config.Config')
+
+app.config['SECRET_KEY'] = '\xb7)\xa8\x9d\xd3\xa1\xeaG[+\xe3\xfa\xe0\xb2\xe2j'
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://allotech:allotech@localhost/ipw'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-from models import Interested
+#from models import Interested
+
+class Interested(db.Model):
+    __tablename__ = 'interested'
+    #__table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Interested {}>'.format(self.time)
+db.create_all()
 
 @app.route('/ipw/')
 def landing():
@@ -29,9 +44,9 @@ def interested():
     row = Interested(time=dt.now())
     db.session.add(row)
     db.session.commit()
+    return 'Change this to be asynchronous js'
 
 if __name__ == "__main__":
 	host = "0.0.0.0"
 	port = 8080
 	app.run(host, port, debug=True)
-
