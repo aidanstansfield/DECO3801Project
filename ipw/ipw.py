84a5ee604190ab656fcd37ce7b2080b87417de66
BenRodgers <benjaminarodgers@gmail.com>
2019-10-10 23:45:23 +1000

    Added in gifs for the ipw landing page

--- ipw/ipw.py
+++ ipw/ipw.py
@@ -15,12 +15,12 @@ db = SQLAlchemy(app)
 # from models import Interested
 
 class Interested(db.Model):
-   __tablename__ = 'interested'
-   #__table_args__ = {'extend_existing': True}
-   id = db.Column(db.Integer, primary_key=True)
-   time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    __tablename__ = 'interested'
+    #__table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
 
-   def __repr__(self):
+    def __repr__(self):
        return '<Interested {}>'.format(self.time)
 db.create_all()
 
