f6edd88d9ab19437fe7218b9c82c8b5bef55311f
Danie Grobbelaar <s4436755@student.uq.edu.au>
2019-10-10 18:11:22 +1000

    Fixed IPW styling. Added form validation and submission.

--- ipw/ipw.py
+++ ipw/ipw.py
@@ -5,24 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime as dt
 
 app = Flask(__name__, static_url_path='/ipw/static/')
-#app.config.from_object('config.Config')
+# app.config.from_object('config.Config')
 
 app.config['SECRET_KEY'] = '\xb7)\xa8\x9d\xd3\xa1\xeaG[+\xe3\xfa\xe0\xb2\xe2j'
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://allotech:allotech@localhost/ipw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db = SQLAlchemy(app)
-#from models import Interested
+db = SQLAlchemy(app)
+# from models import Interested
 
-#class Interested(db.Model):
-#    __tablename__ = 'interested'
-#    #__table_args__ = {'extend_existing': True}
-#    id = db.Column(db.Integer, primary_key=True)
-#    time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-#
-#    def __repr__(self):
-#        return '<Interested {}>'.format(self.time)
-#db.create_all()
+class Interested(db.Model):
+   __tablename__ = 'interested'
+   #__table_args__ = {'extend_existing': True}
+   id = db.Column(db.Integer, primary_key=True)
+   time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+
+   def __repr__(self):
+       return '<Interested {}>'.format(self.time)
+db.create_all()
 
 @app.route('/ipw/')
 def landing():
@@ -41,9 +41,9 @@ def statistics():
 
 @app.route('/ipw/interested', methods=['POST'])
 def interested():
-    #row = Interested(time=dt.now())
-    #db.session.add(row)
-    #db.session.commit()
+    row = Interested(time=dt.now())
+    db.session.add(row)
+    db.session.commit()
     return 'Change this to be asynchronous js'
 
 if __name__ == "__main__":
