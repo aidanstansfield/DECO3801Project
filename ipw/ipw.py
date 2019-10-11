d956342a4102e93eebd808820db1bab74bf63248
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-10-11 14:48:09 +1000

    fixed bad merge

--- ipw/ipw.py
+++ ipw/ipw.py
@@ -7,16 +7,6 @@ from datetime import datetime as dt
 app = Flask(__name__, static_url_path='/ipw/static/')
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
-# from models import Interested
-
-class Interested(db.Model):
-    __tablename__ = 'interested'
-    #__table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-
-    def __repr__(self):
-       return '<Interested {}>'.format(self.time)
 from models import *
 db.create_all()
 
