e1afaddfa15284eaf655b69c663e4ff320149f30
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-10-11 13:57:41 +1000

    graphs yeet

--- ipw/models.py
+++ ipw/models.py
@@ -2,7 +2,6 @@ from ipw import db
 
 class Interested(db.Model):
     __tablename__ = 'interested'
-    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, index=False, unique=False, nullable=False)
     
@@ -10,8 +9,7 @@ class Interested(db.Model):
         return '<Interested {}>'.format(self.time)
 
 class User(db.Model):
-    __tablename__ = 'user'
-    __table_args__ = {'extend_existing': True}
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), index=True, unique=True, nullable=False)
     name = db.Column(db.String(80), index=False, unique=False, nullable=False)
