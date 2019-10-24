a5cbdeed1c2e6758f0c5276e76b9b751a68f6866
Aidan Stansfield <aidan.stansfield@gmail.com>
2019-10-24 10:31:05 +1000

    schema

--- teambuilder/teambuilder/models.py
+++ teambuilder/teambuilder/models.py
@@ -6,8 +6,28 @@ class User(UserMixin, db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,
-                   primary_key=True)
-    username = db.Column(db.String(40),
-                    nullable=False,
-                    unique=True)
+    # NOTE; must be called 'id' for UserMixin goodness
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String, nullable=False, unique=True)
+    courses = db.relationship('Course', backref='users', lazy=True)
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+
+    cid = db.Column(db.Integer, primary_key=True)
+    # if you don't want courses with same name (per user), do some validation 
+    # upon creation
+    name = db.Column(db.String, nullable=False)
+    # a json dump of the questions
+    questions = db.Column(db.Text, nullable=True)
+    uid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    students = db.relationship('Student', backref='courses', lazy=True)
+
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    sid = db.Column(db.String, primary_key=True, nullable=False)
+    cid = db.Column(db.Integer, db.ForeignKey('courses.cid'), primary_key=True)
+    sname = db.Column(db.String, nullable=True) # allow for anon users
+    # a json dump of the survey response
+    response = db.Column(db.Text, nullable=True) 
